714843a08ee98133b89e60be4857f5cdaf718cb5
Amador Navarro <amador_navarro@icloud.com>
2024-11-15 20:54:56 +0100

    Added project

--- Project/Project.swift
+++
@@ -1,7 +0,0 @@
-import ProjectDescription
-import ProjectDescriptionHelpers
-
-let project = Project(name: OReplay.name, organizationName: "com.ruralnerd", targets: OReplay.targets())
-
-/* H6736Y4CV2
- */
--- Project/Tuist/Config.swift
+++
@@ -1,4 +0,0 @@
-import ProjectDescription
-
-@MainActor
-let config = Config(compatibleXcodeVersions: [.upToNextMajor("16.0")], swiftVersion: "6.0")
