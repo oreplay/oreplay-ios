cbfe7d840af247aead7a26e4c284f08d88d7a8ff
Amador Navarro <amador_navarro@icloud.com>
2025-04-23 22:29:25 +0200

    Added large style to event card

--- OReplay/Tests/Presentation/Components/ToolBar/ToolBarViewModelTests.swift
+++ OReplay/Tests/Presentation/Components/ToolBar/ToolBarViewModelTests.swift
@@ -5,25 +5,33 @@ import Testing
 struct ToolBarViewModelTests {
     @Test
     func option_withA11yLabel_shouldKeepThisValue() {
+        // Given
         let sut = ToolBarViewModel.Option(title: "title", a11yLabel: "a11yLabel")
+        // Then
         #expect(sut.a11yLabel == "a11yLabel")
     }
     
     @Test
     func option_withNilA11yLabel_shouldHaveTitleAsValue() {
+        // Given
         let sut = ToolBarViewModel.Option(title: "title")
+        // Then
         #expect(sut.a11yLabel == "title")
     }
     
     @Test
     func option_withA11yIdentifier_shouldKeepThisValue() {
+        // Given
         let sut = ToolBarViewModel.Option(title: "title", a11yIdentifier: "a11yIdentifier")
+        // Then
         #expect(sut.a11yIdentifier == "a11yIdentifier")
     }
     
     @Test
     func option_withNilA11yIdentifier_shouldHaveCustomValue() {
+        // Given
         let sut = ToolBarViewModel.Option(title: "title")
+        // Then
         #expect(sut.a11yIdentifier == "Toolbar_option_title")
     }
 }
